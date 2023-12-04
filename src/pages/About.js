4bedd53deeabcbbbf9d736b05520cfcf42912d57
Paul Balluff <paul.balluff@gmail.com>
2023-12-04 15:23:29 +0100

    #1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,107 @@
+import Markdown from "react-markdown";
+
+const about_text = `# About
+
+OPTED Meteor is a research platform addresses the increasing demand 
+for text analysis tools and training resources among the social science community 
+interested in using political texts. 
+OPTED Meteor organise and link these resources in a systematic way that makes them easy to 
+search, easy to access and easy to utilize. 
+For more details on the development of the research platform please visit [opted.eu](https://opted.eu/) 
+for more information about the project.
+
+Meteor was originally designed by Work Package 3 ([University of Vienna](https://compcommlab.univie.ac.at/)) 
+and further expanded in collaboration with Work Package 9 ([University of Exeter](https://politics.exeter.ac.uk/research/centres/q-step/)) 
+and with the contribution of resources from other OPTED Work Packages:
+
+- 3,944 scientific publications studying Citizen Produced Political Texts (CPPT) were contributed by Work Package 2 .(Gelovani et el, 2021).
+- 129 text resources for texts by political parties were contributed by Work Package 4 (Greene et al, 2021).
+- 195 text resources for legislative texts were contributed by Work Package 5 (Sebők et al, 2021).
+
+# Contact
+You can reach us via various means. If you have feedback or questions you 
+can either send us an email to \`info at opted.eu\` or also poke us with a 
+tweet [@OPTED_H2020](https://twitter.com/OPTED_H2020).
+In case some technical aspect is not working you can raise an issue in our 
+[GitHub repository](https://github.com/opted-eu) or directly send an email to 
+[Paul](https://compcommlab.univie.ac.at/team/praedocs/).
+
+# Contributors
+
+- Principal Investigator: Univ.-Prof. Hajo Boomgaarden, PhD
+- Scientific Team: Dr. Michele Scotto di Vettimo, Paul Balluff, Dr. Fabienne Lind, Marvin Stecker, Celina Dinhopl, Univ.-Prof. Dr. Annie Waldherr, and Prof. Susan Banducci
+- Technical Implementation: Paul Balluff & Ian Wellaway
+
+# References
+
+- Gelovani, S., Kalsnes B., Koc-Michalska, K., and Theocharis, Y. (2021). An inventory of citizen-produced political texts (CPPT): data, tools, and methodology. [OPTED Devliverable (D2.1)](https://opted.eu/results/project-reports/).
+- Greene, Z., Ivanusch, C., Lehmann, P., and Schober T. (2021). A repository of political party and interest group texts. [OPTED Devliverable (D4.2)](https://opted.eu/results/project-reports/).
+- Sebők, M., Proksch, S. and Rauh, C. (2021). Review of available parliamentary corpora. [OPTED Devliverable (D5.1)](https://opted.eu/results/project-reports/).
+- Schwaiger, L. (2022). Gegen die Öffentlichkeit: Alternative Nachrichtenmedien im deutschsprachigen Raum. Bielefeld: transcript Verlag. 10.1515/9783839461211 (List of alternative media for German speaking countries).
+- Michele Scotto di Vettimo, Susan Banducci, Shota Gelovani, Yannis Theocharis, & Laszlo Horvath (2021) Report on a Curation Workflow for the OPTED Platform.
+- Bederke, P., Döring, H., Regel, S. (2023). Party Facts [Dataset]. [partyfacts.org](https://partyfacts.herokuapp.com/)
+
+# Data Licence Agreement
+
+All data listed in Meteor is published under the [CC-BY-SA 4.0 license](https://creativecommons.org/licenses/by-sa/4.0/deed.en). 
+This means you are free to share and adapt Meteor data, 
+as long as you provide proper attribution and use the same license for 
+redistribution or derivate works. 
+
+
+# Citation
+If you are using Meteor as a dataset, please use the following citation:
+
+- Balluff, P., Lind, F., Stecker, M., Dinhopl, C., Boomgaarden, H. G., Waldherr, A. (2022). Meteor: Inventory of news sources, media organizations, and text analysis tools. OPTED. https://meteor.opted.eu
+
+\`\`\`
+@techreport{Meteor,
+    author = {Paul Balluff and Fabienne Lind and Marvin Stecker and 
+        Celina Dinhopl and Hajo G. Boomgaarden and Annie Waldherr},
+    title = {Meteor: Inventory of news sources, media organizations, 
+        and text analysis tools},
+    year = {2022},
+    institution = {OPTED},
+    type = {Database}
+    url = {https://meteor.opted.eu}
+}
+\`\`\`
+
+If you want to reference the Meteor as a platform, please use the following citation:
+
+- Balluff, P., Lind, F., Boomgaarden, H. G., & Waldherr, A. (2022). Mapping the European media landscape – Meteor, a curated and community-coded inventory of news sources. European Journal of Communication. 10.1177/02673231221112006
+
+\`\`\`
+@article{doi:10.1177/02673231221112006,
+    author = {Paul Balluff and Fabienne Lind and 
+        Hajo G. Boomgaarden and Annie Waldherr},
+    title = {Mapping the European media landscape – 
+        Meteor, a curated and community-coded inventory of news sources},
+    venue = {European Journal of Communication},
+    year = {2022},
+    doi = {10.1177/02673231221112006}
+}
+\`\`\`
+
+# Coverage of Meteor
+
+Meteor is a continuously growing database and depends on the contributions 
+of other OPTED Work Packages as well as of individual users. 
+While there is a small team working on adding new entries and reviewing contributions, 
+there is still much work to be done. This registry does not have complete coverage of all 
+political text resources, text analysis tools, and training materials useful for the study 
+of political processes in Europe.
+
+Nevertheless, we encourage all users to contribute entries to Meteor. 
+Even if it is only a few new datasets, inventories, papers or your 
+favourite text analysis tool. If you are not sure where to start, 
+just drop us an email or tweet and we are happy to help.
+
+`
+
 const About = () => {
     return (
-        <>
-            <h1>About</h1>
-            <p>TBA</p>
-        </>
+        <Markdown>{about_text}</Markdown>
     )
 };
 
--- src/pages/PrivacyPolicyAndConsent.js
+++ src/pages/PrivacyPolicyAndConsent.js
@@ -1,5 +1,135 @@
+import Markdown from "react-markdown";
+
+const privacy_text = `
+# Privacy Policy & Consent
+
+## Privacy Policy
+
+This research project is conducted by the University of Vienna which hosts its privacy policy 
+here: [Data Protection Declaration](https://dsba.univie.ac.at/en/privacy-policy).
+
+## Cookies
+
+This website only uses cookies that are technically required to ensure its functionality. 
+There are no cookies for marketing purposes or third-party cookies in place.
+
+## Informed Consent (for project participation)
+
+Thank you for your interest in participating in Meteor!
+
+This data collection effort is part of the EU-funded Project OPTED, 
+where researchers work towards the creation of a new European research 
+infrastructure for the study of political communication in Europe. 
+For this particular platform, we would like to invite you to help us 
+collect information about various European political text resources. 
+Before you start, we would like to draw your attention to a few points. 
+We would kindly ask you to read the following information carefully.
+
+### Purpose of the project
+The ultimate goal is an inventory that helps researchers, journalists, 
+and the interested public to extract in-depth knowledge about the 
+political discourses in Europe. We specifically focus on political texts 
+by various actors, including political parties, legislative texts, news media, 
+as well as texts produced by citizens. While we do not host any text data, 
+we provide references to where such data is available. Furthermore, we also list 
+resources that are useful for studying and analyizing such texts, 
+including tools and learning materials.
+
+We are immensely grateful for your support!
+
+### Contributors’ Rights and Obligations
+
+If you contribute to Meteor, you thereby license it to the public for reuse under [CC BY-SA](https://creativecommons.org/licenses/by-sa/4.0/deed.en).
+ 
+### Procedures
+There is a form that guides the user through the process and prompts 
+for all necessary information. 
+
+### Personal Account as requirement to contribute data
+
+You need to create an account on this platform before you can contribute data 
+to the inventory and before you can fill out the questionnaire. 
+
+Please note the following:
+
+- If you choose to create an account on this platform you need to provide an email address. 
+    We will store your email address for administrative purposes 
+    (i.e., logging in to this platform, administering your account, correspondence strictly necessary for operation). We will not share your email address with any third-parties and it is not publicly visible.
+- We reserve the right to contact our users for administrative purposes 
+    using the provided email address.
+- Any other personal information you add to your account (e.g., your affiliation) 
+    is voluntary and optional.
+- We do not collect sensitive personal information.
+- We reserve the right to publicly display a username next to the information about news sources that you entered. This username is initially randomly generated and assigned automatically to your account after registration. The username is unrelated to the personal information that you provide (i.e. your email address and additional optional information). You can change the username according to your own preferences.
+- We reserve the right to store your IP address for administrative and analytical purposes (e.g. preventing SPAM, or creating usage statistics)
+- If you like you can close your account anytime.
+- The personal information that you provide (i.e. your email address and 
+    additional optional information) will be deleted immediately 
+    if you choose to close your account. The information that you 
+    provide about new sources will remain part of the inventory.
+
+Once you have an account you can contribute data to the inventory:
+
+- The data that you contribute when filling out forms 
+    does not include personal or sensitive information.
+- The data that you contribute when filling out forms may be made available publicly.
+- Before the data is made publicly available, they will be reviewed by an editorial board.
+
+### General
+
+- Data will be collected and processed exclusively on the basis of the legal provisions 
+    (EU General Data Protection Regulation (GDPR), § 7 Abs 2 Datenschutzgesetz (DSG))).
+- This platform uses cookies that are strictly necessary for operation. 
+    We do not use any third-party cookies.
+- Servers where data is stored and processed are in the European Union.
+
+### Potential discomforts and risks
+
+There are no anticipated risks to your participation. 
+When you feel discomfort you can stop filling out the questionnaire anytime.
+
+### Potential benefits to you and/or to society
+The information that you contribute about European political text 
+resources will be part of an inventory. Together with the information 
+added by other contributors the resulting database will serve as a resource for 
+various search interests.
+
+### Compensation for participants
+There is no monetary compensation for your contribution to the coding project.
+
+### Participation and withdrawal
+Co
+ntributing entries is voluntary. You can cancel contributions at any time. 
+The personal information that you provide (email address as a must and 
+additional optional information) will be deleted immediately 
+if you choose to close your account. 
+The entries you made will remain part of the inventory.
+
+### Accessing project outomes
+
+It is likely that articles, reports, and academic publications will be produced 
+based on the information gathered for the inventory. These publications are made 
+publicly available on the OPTED project website: https://www.opted.eu.
+
+### Further information
+
+If you have any questions or concerns about the content, purpose or research 
+ethics of this platform, please feel free to contact the project manager. 
+Should you have comments on the user experience please contact the technical 
+manager Paul Balluff.
+
+All comments or complaints will be treated in strict confidence.
+
+For legal questions in connection with the General Data Protection Regulation (GDPR), 
+please contact the data protection officers of the University of Vienna, 
+Dr. Daniel Stanonik, LL.M. or Dr. Karsten Kinast, LL.M. (verarbeitungsverzeichnis@univie.ac.at). 
+You also have the right to lodge a complaint with the data protection authority 
+(e.g., via dsb@dsb.gv.at).
+
+`
+
 const PrivacyPolicyAndConsent = () => {
-    return <h1>Privacy Policy And Consent</h1>;
+    return <Markdown>{privacy_text}</Markdown>;
 };
 
 export default PrivacyPolicyAndConsent;
